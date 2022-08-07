db61c11f2aafeeb8c9eb879e2265c75e6c44b7fb
Drako <desarrollo3@aveonline.co>
2022-08-07 01:52:13 -0500

    TEST: ok

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,148 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+
+const REGEX = {
+
+  EMAIL: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+
+  TELEFONO: /^(\d{5})((\*{5})|\d{5})|(\*{10})/,
+
+  FECHAS: /^(\d{4})(\/|-)(0[1-9]|1[0-2])(\/|-)([0-2][0-9]|3[0-1])$/
+
+}
+/**
+ * VALIDADOR DE FORMULARIOS ():boolean
+ * @values string o numero a validar
+ */
+export const Validator = (values: string | number) => {
+  const val = values;
+  return {
+    /**
+     * Validador de Strings
+     * @rules > array con las reglas a cumplir
+     * @return Boolean
+     */
+    string(rules: string[] = [] , regex = /()\w+/g): boolean {
+      try {
+        if ( val && typeof val === 'string' ) {
+          if (!val.match(regex)) { return false; }
+          if (rules) {
+            let errs = 0;
+            rules.forEach(r => {
+              const min = r.includes('min') ? true : false;
+              const max = r.includes('max') ? true : false;
+              if (min || max) {
+                const len = Number(r.split(':')[1]);
+                if(min && val.length < len) {
+                  errs++;
+                }
+                if(max && val.length > len) {
+                  errs++;
+                }
+              }
+            }) 
+            return errs > 0 ? false : true;
+          }
+          return true;
+        }
+        return false;
+        
+      } catch (err) {
+        return false;
+      }
+    },
+    /**
+     * Validador de Numbers
+     * @rules > array con las reglas a cumplir
+     * @return Boolean
+     */
+    number(rules: string[] = []): boolean  {
+      try {
+        const valNumber = Number(val);
+        if ( val && valNumber ) {
+          if (rules.length > 1) {
+            let errs = 0;
+            rules.forEach(r => {
+              const min = r.includes('min') ? true : false;
+              const max = r.includes('max') ? true : false;
+              if (min || max) {
+                const len = Number(r.split(':')[1]);
+                if(min && val < len) {
+                  errs++;
+                }
+                if(max && val > len) {
+                  errs++;
+                }
+              }
+            }) 
+            return errs > 0 ? false : true;
+          }
+          return true;
+        }
+        return false;
+      } catch (err) {
+        return false;
+      }
+    },
+    /**
+     * Validador de Emails
+     * @return Boolean
+     */
+    email(): boolean {
+      if ( val && typeof val === 'string' ) {
+        const match = val.match(REGEX.EMAIL);
+        return match ? true : false;
+      }
+      return false;
+    },
+    /**
+     * Validador de Fechas
+     * @return Boolean
+     */
+    date(): boolean {
+      if ( val && typeof val === 'string' ) {
+        const match = val.match(REGEX.FECHAS);
+        return match ? true : false;
+      }
+      return false;
+    },
+     /**
+     * Validador de Telefonos
+     * @return Boolean
+     */
+    phone(): boolean {
+      if ( val && (typeof val === 'string' || typeof val === 'number' )) {
+        const match = String(val).match(REGEX.TELEFONO);
+        return match ? true : false;
+      }
+      return false;
+    },
+     /**
+     * Validador con Expresiones regulares
+     * @reg > Regex a validar con el (method) String.match
+     * @return Boolean
+     */
+    pattern(reg: RegExp): boolean {
+      if ( val && (typeof val === 'string' || typeof val === 'number' )) {
+        const match = String(val).match(reg);
+        return match ? true : false;
+      }
+      return false;
+    },
+     /**
+     * Validador de numeros o strings permitidos
+     * @allow > array con los numeros o strings permitidos
+     * @return Boolean
+     */
+    allowed(allow: number[] | string[]): boolean {
+      if ( val && allow && (typeof val === 'string' || typeof val === 'number' )) {
+        let ok = 0;
+        allow.forEach((el: number | string )=> {
+          if (String(val) == String(el)) {
+            ok++;
+          }
+        })
+        return ok == 1 ? true : false;
+      }
+      return false;
+    }
   }
-  return a + b;
 };
--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,50 @@
-import { sum } from '../src';
+import { Validator } from '../src';
 
-describe('blah', () => {
-  it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
+
+describe('validador de forms Strings', () => {
+  it('string ok', () => {
+    expect(Validator('hola mundo').string()).toEqual(true);
   });
+  it('string error', () => {
+    expect(Validator(66).string()).toEqual(false);
+  });
+});
+
+describe('validador de forms Numbers', () => {
+  it('number', () => {
+    expect(Validator(777).number()).toEqual(true);
+  });
+  it('numberString', () => {
+    expect(Validator('777').number()).toEqual(true);
+  });
+  it('numberString Error', () => {
+    expect(Validator('error').number()).toEqual(false);
+  });
+});
+
+describe('validador de forms Fechas', () => {
+
+  it('dates OK', () => {
+    expect(Validator('2022/08/05').date()).toEqual(true);
+  });
+  it('dates Error', () => {
+    expect(Validator('2022/08/0').date()).toEqual(false);
+  });
+});
+
+describe('validador de forms Valores Permitidos', () => {
+
+  it('allowed OK', () => {
+    expect(Validator(5).allowed([3,5,7,9])).toEqual(true);
+  });
+  it('allowed Error', () => {
+    expect(Validator(2).allowed([3,5,7,9])).toEqual(false);
+  });
+  it('allowed string OK', () => {
+    expect(Validator('tres').allowed(['uno', 'dos', 'tres', 'cuatro'])).toEqual(true);
+  });
+  it('allowed string Error', () => {
+    expect(Validator(2).allowed(['uno', 'dos', 'tres', 'cuatro'])).toEqual(false);
+  });
+
 });
